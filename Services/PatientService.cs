a46072a29abe52dc4061c724218a4a7e1112a86b
PeterHenryy <snakenoxz@gmail.com>
2023-11-07 21:39:20 -0300

    User can now add a credit card

--- Services/PatientService.cs
+++ Services/PatientService.cs
@@ -20,5 +20,29 @@ namespace HospitalApp.Services
             List<Doctor> doctors = _patientRepository.GetDoctorsDropdown();
             return doctors;
         }
+
+        public bool CreateCreditCard(CreditCard creditCard)
+        {
+            bool createdCard = _patientRepository.CreateCreditCard(creditCard);
+            return createdCard;
+        }
+
+        public bool DeleteCreditCard(int creditCardID)
+        {
+            bool updatedCard = _patientRepository.DeleteCreditCard(creditCardID);
+            return updatedCard;
+        }
+
+        public CreditCard GetCreditCardByID(int creditCardID)
+        {
+            CreditCard card = _patientRepository.GetCreditCardByID(creditCardID);
+            return card;
+        }
+
+        public List<CreditCard> GetSpecificUserCards(int userID)
+        {
+            List<CreditCard> userCards = _patientRepository.GetSpecificUserCards(userID);
+            return userCards;
+        }
     }
 }
